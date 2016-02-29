3cf2123c384e74aa5b84f8eb5aef20478c38146f
Trent Hauck <trent@trenthauck.com>
2016-02-29 08:54:43 -0800

    Add doc.go.

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package barneshut is a Barnes Hut simulator. Currently it works for running simulation for the N-Body problem.
+
+Longer-term the goal is to use it for tSNE in go.
+*/
+package barneshut
